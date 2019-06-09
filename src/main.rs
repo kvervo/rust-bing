57a27f905c8974fc5268dbdafedc01c49a5f541b
Supay <37410+kvervo@users.noreply.github.com>
2019-06-09 23:23:13 +0200

    Refactor code and add possibility to save to local 'Wallpapers' folder

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 #[macro_use]
 extern crate serde_derive;
-// #[macro_use]
-// extern crate serde;
-// #[macro_use]
-// extern crate serde_json;
 
 use std::error::Error;
 use reqwest;
 use reqwest::Response;
+use std::fs;
 use std::fs::File;
 use std::path::Path;
+use dirs;
 
 #[derive(Deserialize, Debug)]
 struct Images {
@@ -22,83 +20,92 @@ struct Image {
     startdate: String
 }
 
-fn main() {
-
-    let format = "js";
-    let index = "0";
-    let number = "1";
-    let region = "en-US";
-    let api_url = "http://www.bing.com/HPImageArchive.aspx?\
+const CACHE_DIR: &str = "Wallpapers-rust";
+const FORMAT: &str = "js";
+const INDEX: &str = "0";
+const NUMBER: &str = "1";
+const REGION: &str = "en-US";
+const API_URL: &str = "http://www.bing.com/HPImageArchive.aspx?\
         format={format}&\
         idx={index}&\
         n={number}&\
         mkt={region}";
-    
-    let debug = api_url.replace("{format}",format)
-        .replace("{index}", index)
-        .replace("{number}", number)
-        .replace("{region}", region);
-    
-    let images = match get_json(debug) {
-        Ok(images) => images,
-        Err(_) => panic!("couldn't get Bing Image")
-    };
 
-    match download_image(&images[0]) {
-        Ok(_) => println!("done"),
-        Err(e) => panic!("couldn't set background image to {}", e)
-    }
+fn main() -> std::io::Result<()> {
+    
+    let url = API_URL.replace("{format}",FORMAT)
+        .replace("{index}", INDEX)
+        .replace("{number}", NUMBER)
+        .replace("{region}", REGION);
+    
+    let images = match get_json(url) {
+       Ok(images) => images,
+       Err(_) => panic!("couldn't get Bing Image")
+    };
+    
+    let image_name = format!("image_{}.jpg",&images[0].startdate.as_str());
+    let image_url = format!("http://www.bing.com{}",&images[0].url.as_str());
+
+    match dirs::home_dir().map(|h| h.join(CACHE_DIR)) {
+        Some(cache_dir) => {
+            fs::create_dir_all(&cache_dir)?;
             
-    let image_path = format!("./image_{}.jpg",images[0].startdate.as_str());
-    match set_from_path(&image_path){
-        Ok(result) => println!("{:?}", result),
-        Err(_) => panic!("couldn't set background image to {}", image_path)
+            let image_path = cache_dir.join(image_name);
+            
+            let file = match download_image(&image_url) {
+                Ok(file) => file,
+                Err(e) => panic!("couldn't set background image to {}", e)
+            };
+
+            match save_image(&image_path, file) {
+                Ok(_) => println!("saved"),
+                Err(_) => panic!("couldn't save background image to {:?}", image_path)
+            }
+
+            match set_from_path(&image_path){
+                Ok(result) => println!("{:?}", result),
+                Err(_) => panic!("couldn't set background image to {:?}", image_path)
+            }
+            Ok(())
+        },
+        None => Ok(())
     }
 }
 
 // Request and parse JSON
 fn get_json(url: String) -> Result<Vec<Image>, reqwest::Error>{
-    
     let client = reqwest::Client::new();
     let res = client.get(url.as_str()).send()?.json::<Images>()?;    
     Ok(res.images)
-
 }
 
 // Download file
 // Save image locally
 // Return Image buffer to be set as background
-fn download_image(img: &Image) -> Result<(), reqwest::Error>{
-    let url = format!("http://www.bing.com{}",img.url.as_str());
-    let image_path = format!("./image_{}.jpg",img.startdate.as_str());
-
+fn download_image(url: &String) -> Result<Response, reqwest::Error>{
     let client = reqwest::Client::new();
-    let image_file = client
-        .get(&url)
-        .send()?;
-
-    save_image(image_path, image_file);
-    Ok(())
+    match client.get(url).send() {
+        Ok(file) => Ok(file),
+        Err(why) => panic!("couldn't download {}: {}", url, why),
+    }
 }
-
 
 // Save file locally
-fn save_image(image_path: String, mut image_file: Response){
-    let path = Path::new(&image_path);
-    let display = path.display();
-    let mut file = match File::create(&path) {
-        Err(why) => panic!("couldn't create {}: {}", display, why),
-        Ok(file) => file,
-    };
-    match std::io::copy(&mut image_file, &mut file) {
-        Err(why) => panic!("couldn't write to {}: {}", display, why),
-        Ok(_) => println!("successfully wrote to {}", display)
-    }
+fn save_image(path: &Path, mut file: Response) -> std::io::Result<()> {
     
+    let mut cache_file = match File::create(path) {
+        Ok(file) => file,
+        Err(why) => panic!("couldn't create {}: {}", path.display(), why),
+    };
+
+    match std::io::copy(&mut file, &mut cache_file) {
+        Ok(_) => Ok(()),
+        Err(why) => panic!("couldn't write to {}: {}", path.display(), why),
+    }
 }
 
-fn set_from_path(path: &str) -> Result<(), Box<dyn Error>> {
-    let path = Path::new(&path);
+fn set_from_path(path: &Path) -> Result<(), Box<dyn Error>> {
+    let path = Path::new(path);
     run(
         "osascript",
         &[
